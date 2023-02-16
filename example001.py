f6c650f50f8503a4d044463fd0a564c3f572c2b7
Mikheyev_Denis <mikheydd@mail.ru>
2023-02-16 20:10:47 +0300

    Lec004

---
+++ example001.py
@@ -0,0 +1,21 @@
+# def f(x):
+#     return x * x
+
+# a = f
+# print(a(5))
+
+# def calk1(a, b):
+#     return a + b
+
+
+def calk2(a, b):
+    return a * b
+
+
+def math(op, x, y):
+    print(op(x, y))
+
+
+# calk1 = lambda a, b: a + b
+math(lambda a, b: a + b, 5, 45)
+
